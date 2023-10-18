d34e3e6434aaed99f115810bd8e5b9a2d4cd4198
speedie <speedie@speedie.site>
2023-10-18 19:31:15 +0200

    Change license in header too

--- src/File.cpp
+++ src/File.cpp
@@ -1,6 +1,6 @@
 /* libleet
  * Matrix client library written in C++
- * Licensed under the GNU General Public License version 3.
+ * Licensed under the GNU Affero General Public License version 3.
  * See included LICENSE file for more information.
  *
  * https://git.speedie.site/speedie/libleet
